aa46911aef2e6565965b3779b4dfb191110504f6
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2021-10-08 00:56:36 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
-from app.forms import HeyForm
+from app.forms import HeyForm, HowOld, OrderForm
 
 def root(request):
     return render(request, "root.html")
 
 def hey(request):
-    context = {}
-    if request.method == "GET":
-        form = HeyForm(request.GET)
-        if form.is_valid():
-            context['greating_success'] = True
-    else:
-        form = HeyForm()
-    context['form'] = form
-    return render(request, "hey.html", context)
+    form = HeyForm(request.GET)
+    if form.is_valid():
+        name = form.cleaned_data['name']
+        return render(request, 'hey.html', {"form": form, "name": name})
+    form = HeyForm()
+    return render(request, "hey.html", {"form": form})
+
+def age_in(request):
+    form = HowOld(request.GET)
+    if form.is_valid():
+        year = form.cleaned_data['year']
+        birthyear = form.cleaned_data['birthyear']
+        age = year - birthyear
+        return render(request, "age-in.html", {"form": form, "age": age, "year": year})
+    form = HowOld()
+    return render(request, "age-in.html", {"form": form})
+
+def order(request):
+    form = OrderForm(request.GET)
+    if form.is_valid():
+        burgers = form.cleaned_data['burgers']
+        drinks = form.cleaned_data['drinks']
+        fries = form.cleaned_data['fries']
+        total = (burgers * 4.5) + (fries * 1.5) + drinks
+        return render(request, "order.html", {"form": form, "total": total})
+    form = OrderForm()
+    return render(request, "order.html", {"form": form})
