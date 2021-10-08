f9e13f0f9fa2745bc82f1bfa436b37523dac0d6d
ryleechisholm <89104299+ryleechisholm@users.noreply.github.com>
2021-10-08 00:56:11 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,3 +2,12 @@ from django import forms
 
 class HeyForm(forms.Form):
     name = forms.CharField(max_length=80)
+
+class HowOld(forms.Form):
+    year = forms.IntegerField()
+    birthyear = forms.IntegerField()
+
+class OrderForm(forms.Form):
+    burgers = forms.IntegerField()
+    drinks = forms.IntegerField()
+    fries = forms.IntegerField()
